99e51fe6d54d5da1b0eeca1e0bc58e2a3b5a0f5f
Jiulong Hu <me@hujiulong.com>
2017-09-04 23:44:21 +0800

    refactor: use git/tree/ API instead of the contents/ (reduce API calls)

--- bin/dgit.js
+++ bin/dgit.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
+'use strict'
 
 const program = require( 'commander' )
 const path = require( 'path' )
-const get = require( '../lib/get' )
+const github = require( '../lib/github' )
 
 const log = console.log
 
 program
-    .usage( '<owner/repos/path> [dest]' )
+    .usage( '<owner/repo/path> [dest]' )
     .parse( process.argv )
 
 const source = program.args[ 0 ]
@@ -15,35 +16,35 @@ const rawName = program.args[ 1 ]
 const dest = ( !rawName || rawName === '.' ) ? './' : rawName
 const clone = program.clone || false
 
-const info = parseSource( source );
+const info = parseSource( source )
 
 if ( !info ) {
-    log( 'Error: parameter is illegal.' );
-    log();
+    log( 'Error: parameter is illegal.' )
+    log()
     log( 'Usage:' )
-    log( 'dgit <owner/repos/path> [dest]' );
-    return;
+    log( '    dgit <owner/repo/path> [dest]' )
+    log()
+    return
 }
 
-get( info.owner, info.repo,  normalize( info.path ), normalize( dest ) )
+github( info.owner, info.repo, info.ref, info.path, dest )
 
-function parseSource( path ) {
-    let result;
+function parseSource( source ) {
 
-    if ( !( result = /^((\S+):)?([\w-]+)\/([\w-]+)(\/([\w-\/]*))?(#(\S+))?$/.exec( path ) ) ) return null;
+    let result
+
+    if ( !( result = /^((\S+):)?([\w-]+)\/([\w-]+)(\/([\w-\/]*))?(@(\S+))?$/.exec( source ) ) ) return null
+
+    let path = result[ 6 ] || '/'
+
+    if ( path && path !== '/' && path.charAt( path.length - 1 ) === '/' ) path = path.substring( 0, path.length - 1 )   // src/ -> src
 
     return {
         origin: result[ 2 ] || 'github',
         owner: result[ 3 ],
         repo: result[ 4 ],
-        path: result[ 6 ] || '/',
-        ref: result[ 8 ]
+        path,
+        ref: result[ 8 ] || 'master'
     }
-}
 
-function normalize( source ) {
-    if ( !/(\S)*\.\w+$/.test( source ) && source.charAt( source.length - 1 ) !== '/' ) {
-        source += '/'
-    }
-    return source;
 }
