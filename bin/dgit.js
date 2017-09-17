bd654ad97e5f528c53c5b31bf8145a573010f5a4
Jiulong Hu <me@hujiulong.com>
2017-09-17 17:44:42 +0800

    fix: fix path bug.

--- bin/dgit.js
+++ bin/dgit.js
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
+
 'use strict'
 
 const program = require( 'commander' )
 const path = require( 'path' )
 const github = require( '../lib/github' )
+const version = require( '../package.json' ).version
 
 const log = console.log
 
 program
+    .version( version )
     .usage( '<owner/repo@ref/path> [dest]' )
+    .option( '-r, --ref [value]', 'specify the reference. such as tag, branch or commit hash.' )
     .parse( process.argv )
 
 const source = program.args[ 0 ]
 const rawName = program.args[ 1 ]
 const dest = ( !rawName || rawName === '.' ) ? '' : rawName
-const clone = program.clone || false
+// const clone = program.clone || false
+const ref = program.ref || 'master'
 
 const info = parseSource( source )
 
@@ -27,24 +32,23 @@ if ( !info ) {
     return
 }
 
-github( info.owner, info.repo, info.ref, info.path, dest )
+github( info.owner, info.repo, ref, info.path, dest )
 
 function parseSource( source ) {
 
     let result
 
-    if ( !( result = /^((\S+):)?([\w-]+)\/([\w-\.]+)(\/(\S*))?(@(\S+))?$/.exec( source ) ) ) return null
+    if ( !( result = /^((\S+):)?([\w-]+)\/([\w-\.]+)(\/(\S*))?$/.exec( source ) ) ) return null
 
     let path = result[ 6 ] || ''
 
-    if ( path && path !== '' && path.charAt( path.length - 1 ) === '/' ) path = path.substring( 0, path.length - 1 )   // src/ -> src
+    if ( path && path !== '' && path.charAt( path.length - 1 ) === '/' ) path = path.substring( 0, path.length - 1 ) // src/ -> src
 
     return {
         origin: result[ 2 ] || 'github',
         owner: result[ 3 ],
         repo: result[ 4 ],
-        path,
-        ref: result[ 8 ] || 'master'
+        path
     }
 
 }
