b93f38e197b5490c468b0d29e8aee69ab0619960
Jiulong Hu <me@hujiulong.com>
2018-12-27 13:38:02 +0800

    style: lint code

--- bin/dgit.js
+++ bin/dgit.js
@@ -1,54 +1,57 @@
 #!/usr/bin/env node
 
-'use strict'
+const program = require('commander');
+const github = require('../lib/github');
+const version = require('../package.json').version;
 
-const program = require( 'commander' )
-const path = require( 'path' )
-const github = require( '../lib/github' )
-const version = require( '../package.json' ).version
+/* eslint-disable no-console */
 
-const log = console.log
+const log = console.log;
 
-program
-    .version( version )
-    .usage( '<owner/repo@ref/path> [dest]' )
-    .option( '-r, --ref [value]', 'specify the reference. such as tag, branch or commit hash.' )
-    .parse( process.argv )
+function parseSource(source) {
+  const result = /^((\S+):)?([\w-]+)\/([\w-\.]+)(\/(\S*))?$/.exec(source);
 
-const source = program.args[ 0 ]
-const rawName = program.args[ 1 ]
-const dest = ( !rawName || rawName === '.' ) ? '' : rawName
-// const clone = program.clone || false
-const ref = program.ref || 'master'
+  if (!result) return null;
 
-const info = parseSource( source )
+  let path = result[6] || '';
 
-if ( !info ) {
-    log( 'Error: parameter is illegal.' )
-    log()
-    log( 'Usage:' )
-    log( '    dgit <owner/repo@ref/path> [dest]' )
-    log()
-    return
+  if (path && path !== '' && path.charAt(path.length - 1) === '/') path = path.substring(0, path.length - 1); // src/ -> src
+
+  return {
+    origin: result[2] || 'github',
+    owner: result[3],
+    repo: result[4],
+    path,
+  };
 }
 
-github( info.owner, info.repo, ref, info.path, dest )
+function excute() {
+  program
+    .version(version)
+    .usage('<owner-535/repo@ref/path> [dest]')
+    .option('-r, --ref [value]', 'specify the reference. such as tag, branch or commit hash.')
+    .parse(process.argv);
 
-function parseSource( source ) {
+  const source = program.args[0];
+  const rawName = program.args[1];
+  const dest = (!rawName || rawName === '.') ? '' : rawName;
+  // const clone = program.clone || false
+  const ref = program.ref || 'master';
 
-    let result
+  const info = parseSource(source);
 
-    if ( !( result = /^((\S+):)?([\w-]+)\/([\w-\.]+)(\/(\S*))?$/.exec( source ) ) ) return null
+  if (!info) {
+    log('Error: parameter is illegal.');
+    log();
+    log('Usage:');
+    log('    dgit <owner/repo@ref/path> [dest]');
+    log();
+    return;
+  }
 
-    let path = result[ 6 ] || ''
-
-    if ( path && path !== '' && path.charAt( path.length - 1 ) === '/' ) path = path.substring( 0, path.length - 1 ) // src/ -> src
-
-    return {
-        origin: result[ 2 ] || 'github',
-        owner: result[ 3 ],
-        repo: result[ 4 ],
-        path
-    }
-
+  if (info.origin === 'github') {
+    github(info.owner, info.repo, ref, info.path, dest);
+  }
 }
+
+excute();
